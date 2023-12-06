379b8116ccb4594363ad96a34bc5830f5b78ace5
JuanSaet <jumasaet@espol.edu.ec>
2023-12-06 00:42:41 -0500

    Se añadió una función para que dependiendo del string y el ángulo que se reciba controle el servo específico de dicho string, además se añadió una función encargada de identificar qué slider tiene cambios para asociarlo al servo correspondiente

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -19,8 +19,15 @@
 
 
 #define LED_PIN 12  // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+#define RangoError 40
 
 Servo servo_garra;
+Servo servo_muneca_pitch;
+Servo servo_muneca_yaw;
+Servo servo_codo;
+Servo servo_antebrazo;
+Servo servo_base;
+
 // Set these to your desired credentials.
 const char *ssid = "Brazo Robotico";
 const char *password = "12345678";
@@ -29,14 +36,44 @@ int intentosFallidos = 0;
 
 WiFiServer server(80);
 
-void controlarServo(int angulo){
-   // Ajustar el rango del ángulo según tus necesidades
+void moverServo(int angulo, String servo){
+  
+  // Ajustar el rango del ángulo según tus necesidades
   int anguloAjustado = constrain(angulo, 0, 180);
 
-  // Mover el servo al ángulo especificado
-  Serial.print(anguloAjustado);
-  servo_garra.write(anguloAjustado);
+  if (servo == "garra"){
+    Serial.println(anguloAjustado);
+    servo_garra.write(anguloAjustado);
+  }
+
+  if (servo == "muneca_pitch"){
+    Serial.println(anguloAjustado);
+    servo_muneca_pitch.write(anguloAjustado);
+  }
+  
+  if (servo == "muneca_yaw"){
+    Serial.println(anguloAjustado);
+    servo_muneca_yaw.write(anguloAjustado);
+  }
+  
+  if (servo == "codo"){
+    Serial.println(anguloAjustado);
+    servo_codo.write(anguloAjustado);
+  }
+  
+  if (servo == "antebrazo"){
+    Serial.println(anguloAjustado);
+    servo_antebrazo.write(anguloAjustado);
+  }
+
+  if (servo == "base"){
+    Serial.println(anguloAjustado);
+    servo_base.write(anguloAjustado);
+  }
+
 }
+
+
 void controlarLED(int valor){
   int brillo = map(valor, 0, 180, 0, 255);  // Mapear el valor del ángulo a un valor de brillo para el LED
   analogWrite(LED_PIN, brillo);             // Establecer el brillo del LED
@@ -44,9 +81,155 @@ void controlarLED(int valor){
   Serial.println(brillo);
 }
 
+
+void controlarServos(String line){
+
+  if      (line.startsWith("GET /control?garra=") and line.endsWith("HTTP/1.1")){
+          // Extraer el valor del servomotor de la solicitud
+          int pos = line.indexOf('=');
+
+          if (pos != -1) {
+            int valorServomotor = line.substring(pos + 1).toInt();
+
+            // Filtrar valores incorrectos
+            if ((abs(valorServomotor - ultimoValor)) < RangoError) {
+              moverServo(valorServomotor,"garra");
+              Serial.printf("\n\nÚltimo valor de la garra: %d\n",ultimoValor);
+
+              if(valorServomotor != 0){
+                ultimoValor = valorServomotor;
+                intentosFallidos = 0;
+                Serial.printf("El valor de la garra: %d\n", valorServomotor);
+              }
+            } else {
+              intentosFallidos++;
+            }
+          }
+  }
+  else if (line.startsWith("GET /control?muneca_pitch=") and line.endsWith("HTTP/1.1")){
+          // Extraer el valor del servomotor de la solicitud
+          int pos = line.indexOf('=');
+
+          if (pos != -1) {
+            int valorServomotor = line.substring(pos + 1).toInt();
+
+            // Filtrar valores incorrectos
+            if ((abs(valorServomotor - ultimoValor)) < RangoError) {
+              moverServo(valorServomotor,"muneca_pitch");
+              Serial.printf("\n\nÚltimo valor de muneca_pitch: %d\n\n",ultimoValor);
+
+              if(valorServomotor != 0){
+                ultimoValor = valorServomotor;
+                intentosFallidos = 0;
+                Serial.printf("El valor de muneca_pitch: %d\n", valorServomotor);
+              }
+            } else {
+              intentosFallidos++;
+            }
+          }
+  }
+  else if (line.startsWith("GET /control?muneca_yaw=") and line.endsWith("HTTP/1.1")){
+          // Extraer el valor del servomotor de la solicitud
+          int pos = line.indexOf('=');
+
+          if (pos != -1) {
+            int valorServomotor = line.substring(pos + 1).toInt();
+
+            // Filtrar valores incorrectos
+            if ((abs(valorServomotor - ultimoValor)) < RangoError) {
+              moverServo(valorServomotor,"muneca_yaw");
+              Serial.printf("\n\nÚltimo valor muneca_yaw: %d\n\n",ultimoValor);
+
+              if(valorServomotor != 0){
+                ultimoValor = valorServomotor;
+                intentosFallidos = 0;
+                Serial.printf("El valor de muneca_yaw: %d\n", valorServomotor);
+              }
+            } else {
+              intentosFallidos++;
+            }
+          }
+  }
+  else if (line.startsWith("GET /control?codo=") and line.endsWith("HTTP/1.1")){
+          // Extraer el valor del servomotor de la solicitud
+          int pos = line.indexOf('=');
+
+          if (pos != -1) {
+            int valorServomotor = line.substring(pos + 1).toInt();
+
+            // Filtrar valores incorrectos
+            if ((abs(valorServomotor - ultimoValor)) < RangoError) {
+              moverServo(valorServomotor,"codo");
+              Serial.printf("\n\nÚltimo valor de codo: %d\n\n",ultimoValor);
+
+              if(valorServomotor != 0){
+                ultimoValor = valorServomotor;
+                intentosFallidos = 0;
+                Serial.printf("El valor de codo: %d\n", valorServomotor);
+              }
+            } else {
+              intentosFallidos++;
+            }
+          }
+  }
+  else if (line.startsWith("GET /control?antebrazo=") and line.endsWith("HTTP/1.1")){
+          // Extraer el valor del servomotor de la solicitud
+          int pos = line.indexOf('=');
+
+          if (pos != -1) {
+            int valorServomotor = line.substring(pos + 1).toInt();
+
+            // Filtrar valores incorrectos
+            if ((abs(valorServomotor - ultimoValor)) < RangoError) {
+              moverServo(valorServomotor,"antebrazo");
+              Serial.printf("\n\nÚltimo valor de antebrazo: %d\n\n",ultimoValor);
+
+              if(valorServomotor != 0){
+                ultimoValor = valorServomotor;
+                intentosFallidos = 0;
+                Serial.printf("El valor de antebrazo: %d\n", valorServomotor);
+              }
+            } else {
+              intentosFallidos++;
+            }
+          }
+  }
+  else if (line.startsWith("GET /control?base=") and line.endsWith("HTTP/1.1")){
+          // Extraer el valor del servomotor de la solicitud
+          int pos = line.indexOf('=');
+
+          if (pos != -1) {
+            int valorServomotor = line.substring(pos + 1).toInt();
+
+            // Filtrar valores incorrectos
+            if ((abs(valorServomotor - ultimoValor)) < RangoError) {
+              moverServo(valorServomotor,"base");
+              Serial.printf("\n\nÚltimo valor de base: %d\n\n",ultimoValor);
+
+              if(valorServomotor != 0){
+                ultimoValor = valorServomotor;
+                intentosFallidos = 0;
+                Serial.printf("El valor de base: %d\n", valorServomotor);
+              }
+            } else {
+              intentosFallidos++;
+            }
+          }
+  }
+
+}
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   servo_garra.attach(3);
+  /*
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+  Falta hacer el attach los pines para cada una de los servos
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+  */
 
   Serial.begin(115200);
   Serial.println();
@@ -76,39 +259,15 @@ void loop() {
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        }else if (c != '\r'){  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
 
         if (currentLine.startsWith("GET /control?garra=") and currentLine.endsWith("HTTP/1.1")){
           // Extraer el valor del servomotor de la solicitud
           int pos = currentLine.indexOf('=');
+
           if (pos != -1) {
             int valorServomotor = currentLine.substring(pos + 1).toInt();
             // Filtrar valores incorrectos
-            if ((abs(valorServomotor - ultimoValor)) < 40) {
+            if ((abs(valorServomotor - ultimoValor)) < RangoError) {
               controlarLED(valorServomotor);
               Serial.print("El ultimo valor: ");
               Serial.println(ultimoValor);
